a6445c103f50b05388b85cb5d99989a286b352d5
arslanefimov <dns230717rus@gmail.com>
2025-11-05 20:52:17 +0300

    feat(server-config) added minimum working version of server config

---
+++ user-service/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.7"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.itmowork"
+version = "0.0.1-SNAPSHOT"
+description = "User service"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.0.0"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
