018479625c665aae2be0cb30a808b309f1ab31b0
arslanefimov <dns230717rus@gmail.com>
2025-11-19 23:32:27 +0300

    feat(user-service) added main logic to user-service: create-user and exists user by id endpoints

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -21,10 +21,19 @@ repositories {
 extra["springCloudVersion"] = "2025.0.0"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	implementation("org.liquibase:liquibase-core:5.0.1")
+	implementation("org.apache.commons:commons-lang3:3.18.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.8.13")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("org.postgresql:postgresql")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	testImplementation("io.projectreactor:reactor-test")
+//	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
